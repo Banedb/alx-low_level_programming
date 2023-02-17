148ff69085afec2028fcce907a201d51895985a6
Bane <marcytornad@gmail.com>
2023-02-16 18:15:58 -0800

    hhj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main()
+int main(void)
+/*
+ *Main(): where the execution begins
+ *If code returns 0, execution was successful
+ */
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }

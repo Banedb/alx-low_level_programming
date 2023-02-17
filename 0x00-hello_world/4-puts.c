007af6986261a0dd8781fa2b86fa6f66c648d466
Bane <marcytornad@gmail.com>
2023-02-16 18:24:50 -0800

    gkkk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
-/*
- *Main(): where the execution begins
- *If code returns 0, execution was successful
+/**
+ *Main - where the execution begins
+ *If code returns 0, execution was successful*
  */
 {
   puts("\"Programming is like building a multilingual puzzle");

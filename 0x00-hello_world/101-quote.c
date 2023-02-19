03b11bd06f70f3b9fd5efc3a03fe126955152317
Bane <marcytornad@gmail.com>
2023-02-19 13:50:22 -0800

    stderr msg not using puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: 1 (Error)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-return (1);
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (-1);
+	return (0);
 }

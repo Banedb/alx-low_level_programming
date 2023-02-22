d4d974e3ed5d99d4893b0cd90865002e72ca823a
Bane <marcytornad@gmail.com>
2023-02-21 16:48:02 -0800

    printing natural numbers

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,26 @@
 void print_alphabet(void);
 /* Prototype for num2 */
 void print_alphabet_x10(void);
+/* Prototype for num3 */
+int _islower(int c);
+/* Prototype for num4 */
+int _isalpha(int c);
+/* Prototype for num5 */
+int print_sign(int n);
+/* Prototype for num6 */
+int _abs(int);
+/* Prototype for num7 */
+int print_last_digit(int);
+/* Prototype for num8 */
+void jack_bauer(void);
+/* Prototype for num9 */
+void time_teble(void);
+/* Prototype for num10 */
+int add(int, int);
+/* Prototype for num11 */
+void print_to_98(int n);
+/* Prototype for num2 */
+void print_times_table(int n);
 /* Prototype for _putchar function */
 int _putchar(char c);
 #endif

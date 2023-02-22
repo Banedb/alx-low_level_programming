2726897f0ee4bf08229d8f7e2250912c1bc62453
Bane <marcytornad@gmail.com>
2023-02-21 16:03:43 -0800

    printing 10X

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* Prototype for printing alphabet function */
+/* Prototype for num1 */
 void print_alphabet(void);
+/* Prototype for num2 */
+void print_alphabet_x10(void);
 /* Prototype for _putchar function */
 int _putchar(char c);
 #endif

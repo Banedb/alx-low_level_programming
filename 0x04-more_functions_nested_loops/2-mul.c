2eb0d7f964288aa9b9653c5bc53bee36ec26dc08
Bane <marcytornad@gmail.com>
2023-02-23 15:55:51 -0800

    printing triangle

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Multiplies 2 integers
+ *
+ * Return: Return the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

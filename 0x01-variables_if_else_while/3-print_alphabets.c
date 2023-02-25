3fd1ca5c0e236e6e58d419314f2be6ab7cf58d6d
Bane <marcytornad@gmail.com>
2023-02-25 10:32:31 -0800

    printing ALFS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Prints the alphabet.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
+	char alf, ALF;
 
-for (i = 0; i < 52; i++)
-{
-putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+	for (alf = 'a'; alf < 'z' + 1; alf++)
+		putchar(alf);
+	for (ALF = 'A'; ALF < 'Z' + 1; ALF++)
+		putchar(ALF);
+	putchar('\n');
+	return (0);
 }

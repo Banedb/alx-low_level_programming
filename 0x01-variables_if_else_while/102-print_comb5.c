2d887577d461eec69e6b7d283f49633447fb1ef4
Bane <marcytornad@gmail.com>
2023-02-26 08:57:36 -0800

    printing 00-99 combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,39 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int num1, num2, num3, num4;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-g = 48;
-while (g < 58)
-{
-e = 48;
-while (e < 58)
-{
-i = 48;
-while (i < 58)
-{
-op1 = (h * 10) + g;
-op2 = (e * 10) + i;
-if (op1 < op2)
-{
-putchar(h);
-putchar(g);
-putchar(' ');
-putchar(e);
-putchar(i);
-if (h == 57 && g == 56 && e == 57 && i == 57)
-break;
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-e++;
-}
-g++;
-}
-h++;
-}
-putchar('\n');
-return (0);
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			for (num3 = 0; num3 <= 9; num3++)
+			{
+				for (num4 = 0; num4 <= 9; num4++)
+				{
+					if (num1 >= num3 && num2 >= num4)
+						continue;
+					if (num1 > num3)
+						continue;
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar (' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
+					{
+						putchar(',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

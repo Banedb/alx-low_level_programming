c65522fec1f4414fc1a8f8a169b45d0ed8172895
Bane <marcytornad@gmail.com>
2023-02-26 12:27:57 -0800

    Redoing main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,29 +1,29 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* Prototype for num1 */
-void print_alphabet(void);
-/* Prototype for num2 */
-void print_alphabet_x10(void);
-/* Prototype for num3 */
-int _islower(int c);
-/* Prototype for num4 */
-int _isalpha(int c);
-/* Prototype for num5 */
-int print_sign(int n);
-/* Prototype for num6 */
-int _abs(int);
-/* Prototype for num7 */
-int print_last_digit(int);
-/* Prototype for num8 */
-void jack_bauer(void);
-/* Prototype for num9 */
-void times_table(void);
-/* Prototype for num10 */
-int add(int, int);
-/* Prototype for num11 */
-void print_to_98(int n);
-/* Prototype for num12 */
-void print_times_table(int n);
-/* Prototype for _putchar function */
+/* Prototype for _putchar */
 int _putchar(char c);
-#endif
+/* Prototype for #1 */
+void print_alphabet(void);
+/* Prototype for #2 */
+
+/* Prototype for #3 */
+
+/* Prototype for #4 */
+
+/* Prototype for #5 */
+
+/* Prototype for #6 */
+
+/* Prototype for #7 */
+
+/* Prototype for #8 */
+
+/* Prototype for #9 */
+
+/* Prototype for #10 */
+
+/* Prototype for #11 */
+
+/* Prototype for #1 */
+
+/* Prototype for #1 */
+
+/* Prototype for #1 */

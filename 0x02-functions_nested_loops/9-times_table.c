436f85528aa75ffb97972dbf37f832532fa7492b
Bane <marcytornad@gmail.com>
2023-03-01 09:52:02 -0800

    9 timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,36 @@
 #include "main.h"
+
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - prints 9 timestable
+ * takes no argument
+ *
+ * Return: void
  */
 void times_table(void)
 {
-	int i, j, k;
+	int a, b, c;
 
-	for (i = 0; i < 10; i++)
+	for (a = 0; a < 10; a++)
 	{
-		for (j = 0; j < 10; j++)
+		for (b = 0; b < 10; b++)
 		{
-			k = j * i;
-			if (j == 0)
+			c = a * b;
+			if (c >= 10)
 			{
-				_putchar(k + '0');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
-			if (k < 10 && j != 0)
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + c);
+			}
+			if (b != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
 			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+
 		}
 		_putchar('\n');
 	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,13 +11,13 @@ int _isalpha(int c);
 /* Prototype for #5 */
 int print_sign(int n);
 /* Prototype for #6 */
-
+int _abs(int);
 /* Prototype for #7 */
-
+int print_last_digit(int);
 /* Prototype for #8 */
-
+void jack_bauer(void);
 /* Prototype for #9 */
-
+void times_table(void);
 /* Prototype for #10 */
 
 /* Prototype for #11 */

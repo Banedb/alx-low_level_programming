3d655ebbb032a759155e14868e909bcd627c89f5
Bane <marcytornad@gmail.com>
2023-03-07 08:05:40 -0800

    pointers and address

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
-	}
-	printf("\n");
+	for (i = 0; &a[i] != &a[n]; i++)
+		if (&a[i] != (&a[n - 1]))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 }

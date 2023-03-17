ec0e94fcc5d9f17871098784d83b15195fcbb3a1
Bane <marcytornad@gmail.com>
2023-03-16 18:22:48 -0700

    Multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include "main.h"
 /**
  * main - multiply 2 numbers as arguments
  * @argc: number of arguments
  * @argv: arguments
- * Return: 0 success
+ *
+ * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
@@ -13,8 +14,10 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	if (argc != 3)
-	{ printf("Error\n");
-		exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)

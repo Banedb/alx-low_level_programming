28b0c4b504d17680fc5fb4555dea45fb22e4834b
Bane <121007715+Banedb@users.noreply.github.com>
2023-03-19 13:28:57 +0100

    Update 6-cap_string.c
    First caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,13 +22,9 @@ char *cap_string(char *s)
 			s[i] = s[i] - 32;
 		}
 		else if ((s[0] >= 97 && s[0] <= 122))
-		{
 			s[0] = s[0] - 32;
-		}
 		else
-		{
 			s[i] = s[i];
-		}
 		i++;
 	}
 	return (s);

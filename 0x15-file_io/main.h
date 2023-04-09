184f3ec450d68f943153d1b7c8a824dab02ccef8
Bane <marcytornad@gmail.com>
2023-04-09 09:38:04 -0700

    bits

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -22,5 +22,6 @@ void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+int _putchar(char c);
 void close_elf(int elf);
 #endif

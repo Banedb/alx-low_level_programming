4d08ed930898c1c0d50efd495bf1de2613cf27bf
Bane <marcytornad@gmail.com>
2023-04-09 10:24:29 -0700

    io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,27 +6,25 @@
  *
  * Return: 1 (Success) || -1 (Failure)
  */
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+int create_file(const char *filename, char *text_content)
+{
+	int file, len, bw;
 
-int create_file(const char *filename, char *text_content) {
-	if (!filename) {
-		return -1;
-	}
-	int fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
-	if (fd == -1) {
-		return -1;
-	}
-	if (text_content) {
-		int len = strlen(text_content);
-		int bytes_written = write(fd, text_content, len);
-		if (bytes_written != len) {
-			close(fd);
-			return -1;
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content)
+	{
+		len = strlen(text_content);
+		bw = write(file, text_content, len);
+		if (bw != len)
+		{
+			close(file);
+			return (-1);
 		}
 	}
-	close(fd);
-	return 1;
+	close(file);
+	return (1);
 }

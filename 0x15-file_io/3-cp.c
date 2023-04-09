70a886f81a0e7f91cd77e9684eb1d308b210a5d5
Bane <marcytornad@gmail.com>
2023-04-09 11:31:28 -0700

    io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	}
 	if (x < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	m = close(fr);

05d16c4f796f573f1e64c44c13e01fe91f3c077e
Bane <marcytornad@gmail.com>
2023-04-16 08:27:12 -0700

    Leet encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
  * leet -  encodes a string into 1337.
- * @s: string to code
+ * @str: string to code
  *
  * Return: the encoded string
  */
 char *leet(char *str)
 {
 	int i, j;
-	char num[] = {'4', '3', '0', '7', '1'};
-	char alf[] = {'a', 'e', 'o', 't', 'l'};
-	char ALF[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = "4433007711";
+	char alfs[] = "AaEeOoTtLl";
 
 	for (i = 0; str[i] != '\0'; i++)
 		for (j = 0; j < 5; j++)
-			if ((str[i] == alf[j]) || (str[i] == ALF[j]))
+			if (str[i] == alfs[j])
 				str[i] = num[j];
 	return (str);
 }

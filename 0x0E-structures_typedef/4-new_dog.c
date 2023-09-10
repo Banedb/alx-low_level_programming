ddca4e2356b2ad3824d52f3c6f4832c5b0ef9f72
Bane <marcytornad@gmail.com>
2023-09-10 16:40:05 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
 
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-
 /**
  * new_dog - creates a new dog
  * @name: the dog's name
@@ -13,75 +10,27 @@ char *_strcpy(char *dest, char *src);
  * Return: NULL (Failure) || cutie_dog (Success)
  */
 dog_t *new_dog(char *name, float age, char *owner)
+
 {
-	dog_t *cutie_dog;
-	int name_l = 0, own_l = 0;
+	dog_t *dogcraze;
+	int i, j;
+	char *nc, *oc;
 
-	if (name != NULL && owner != NULL)
-	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		cutie_dog = malloc(sizeof(dog_t));
+	nc = malloc(sizeof(name));
+	oc = malloc(sizeof(owner));
+	dogcraze = malloc(sizeof(dog_t));
 
-		if (cutie_dog == NULL)
-			return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		nc[i] = name[i];
+	nc[i] = name[i];
+	for (j = 0; name[j] != '\0'; j++)
+		oc[j] = owner[j];
+	oc[j] = owner[j];
+	if (!dogcraze)
+		return (NULL);
 
-		cutie_dog->name = malloc(sizeof(char) * name_l);
-
-		if (cutie_dog->name == NULL)
-		{
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->owner = malloc(sizeof(char) * own_l);
-
-		if (cutie_dog->owner == NULL)
-		{
-			free(cutie_dog->name);
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->name = _strcpy(cutie_dog->name, name);
-		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
-		cutie_dog->age = age;
-	}
-
-	return (cutie_dog);
-}
-
-/**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
- */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	for (; *s != '\0'; s++)
-		c++;
-
-	return (c);
-}
-
-/**
- * _strcpy - Copy a string
- * @dest: Destination value
- * @src: Source value
- *
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	dest[i++] = '\0';
-
-	return (dest);
+	dogcraze->name = name;
+	dogcraze->age = age;
+	dogcraze->owner = owner;
+	return (dogcraze);
 }

e63cf77c7dc054bb4caa7169d891025d7255cb1c
Bane <121007715+Banedb@users.noreply.github.com>
2025-03-21 22:48:47 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,23 +10,13 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *temp, *node = malloc(sizeof(dlistint_t));
+	dlistint_t *node = malloc(sizeof(dlistint_t));
 
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
 	node->prev = NULL;
-	node->next = NULL;
-
-	if (*head == NULL)
-	{
-		node->next = NULL;
-		*head = node;
-		return (*head);
-	}
-	temp = *head;
-	node->next = temp;
-	temp->prev = node;
+	node->next = *head;
 	*head = node;
 	return (node);
 }

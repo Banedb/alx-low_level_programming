a5205da67e3c45d59c43445277c582ff63c11913
Bane <121007715+Banedb@users.noreply.github.com>
2025-03-31 16:12:27 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/del https://github.com/Banedb/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp/del https://raw.githubusercontent.com/Banedb/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="/tmp/del/libgiga.so"
